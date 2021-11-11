38b5b8c4ad481b1c7e6152762ab2ac12f4288802
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-11 11:05:19 +0100

    style done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,11 @@ import {HttpClient} from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 // declare let switchCtn:any;
-declare  var jQuery:  any;
+// declare  function getButtons () :  void;
+// declare  function mainF () :  void;
+
+// declare  function changeForm () :  void;
+// declare  function e () :  void;
 
 @Component({
   selector: 'app-login',
@@ -15,8 +19,8 @@ export class LoginComponent implements OnInit {
 
   public loginForm !: FormGroup
 
-  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router) { }
-
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router) {  }
+ 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       email:[''],
@@ -24,10 +28,11 @@ export class LoginComponent implements OnInit {
     })
   }
 
-  login(){
 
-    
-    this.http.get<any>("http://localhost:3000/signupUsers")
+  
+
+  login(){
+   this.http.get<any>("http://localhost:3000/signupUsers")
     .subscribe(res=>{
       const user= res.find((a:any)=>{
         return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
@@ -44,10 +49,11 @@ export class LoginComponent implements OnInit {
     },err=>{
       alert("Erreur")
     })
-
+   
     
     
   }
+ 
 
 }
 export class SignupComponent implements OnInit {
@@ -55,7 +61,8 @@ export class SignupComponent implements OnInit {
   public signupForm !: FormGroup;
 
   constructor(private formBuilder : FormBuilder, private http: HttpClient, private router:Router, private authService: AuthService) { }
-
+ 
+  
   ngOnInit() : void {
     this.signupForm = this.formBuilder.group ({
         username : [''],
@@ -75,6 +82,5 @@ export class SignupComponent implements OnInit {
     }
     )
   }
-
+  
 }
-
