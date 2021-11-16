0d65bf2eb169845278151d10d8437ffdd23d389f
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-16 13:13:12 +0100

    login api work

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router =express.Router()
 const User =require ('../models/user')
 const mongoose = require ('mongoose')
+const user = require('../models/user')
 const db = "mongodb+srv://ah-user:ah-user@cluster0.y8kt0.mongodb.net/auth-db?retryWrites=true&w=majority"
 mongoose.connect(db, err=>{
     if (err){
@@ -29,6 +30,29 @@ router.post ('/register',(req,res)=> {
     
 })
 })
+
+// login api
+router.post('/login', (req,res)=>{
+    let userData =req.body //extract the user data
+
+    user.findOne({email:userData.email},(error, user)=>{
+        if (error) {
+            console.log(error)
+        } else {
+            if (!user){
+                res.status(401).send('Invalid email')
+            }else 
+            if (user.password !== userData.password){
+                res.status(401).send('Invalid Password')
+            }else {
+                res.status(200).send(user)
+            }
+        }
+    })
+
+
+})
+
 
 
 module.exports = router
