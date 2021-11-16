2352e94a335ea6e05a0181cdf25f4be36b6ba836
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-16 10:43:36 +0100

    initial commit "express server"

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require ('body-parser')
+
+const PORT =3000
+const api =require ('./routes/api')
+const app = express()
+
+app.use(bodyParser.json())
+app.use('/api',api)
+
+app.get('/',function(req,res){
+  res.send('HELLO FROM SERVER')
+})
+
+app.listen(PORT,function(){
+ console.log('server running on localhost:' +PORT)
+
+})
