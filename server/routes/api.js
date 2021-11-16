3e9502c8a17ab4bd29e2291b755f5e17da9ff5a6
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-16 12:08:51 +0100

    connected to mongodb "done"

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router =express.Router()
 
+const mongoose = require ('mongoose')
+const db = "mongodb+srv://ah-user:ah-user@cluster0.y8kt0.mongodb.net/auth-db?retryWrites=true&w=majority"
+mongoose.connect(db, err=>{
+    if (err){
+        console.error('Error!'+ err)
+    }else {
+
+        console.log ('connected to mongodb')
+    }
+})
+
 router.get('/', (req,res) => {
     res.send('From Api route')
 })
