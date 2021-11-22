65713e97bf1505f6b9bca031bace30aed8ff99c4
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-11-22 09:48:25 +0100

    add json web token code

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt =require ('jsonwebtoken')
 const router =express.Router()
 const User =require ('../models/user')
 const mongoose = require ('mongoose')
@@ -25,7 +26,9 @@ router.post ('/register',(req,res)=> {
     if(error){
         console.log(error)
     }else{
-        res.status(200).send(registeredUser)
+        let payload ={ subject: registeredUser._id}
+        let token = jwt.sign(payload,'secretKey')
+        res.status(200).send({token})
     }
     
 })
@@ -45,7 +48,9 @@ router.post('/login', (req,res)=>{
             if (user.password !== userData.password){
                 res.status(401).send('Invalid Password')
             }else {
-                res.status(200).send(user)
+                let payload = { subject: user._id}
+                let token = jwt.sign(payload, 'secretKey')
+                res.status(200).send({user})
             }
         }
     })
